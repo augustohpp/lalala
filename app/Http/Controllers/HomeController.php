c3e5e6ac192b57a4cc937763a484591dd6f00721
augustohpp <augustorodprograma@gmail.com>
2020-12-10 20:41:40 -0800

    l

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
+    /**ppk
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
